1aae8feb9a2ff72dcec4d9a9efbc1bda84585e78
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-23 07:50:47 +0200

    Django tutorial step 02

--- christmasflix/urls.py
+++ christmasflix/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:list_id>/', views.detail, name='detail'),
+    path('<int:list_id>/results/', views.results, name='results'),
+    path('<int:list_id>/vote/', views.vote, name='vote')
 ]
--- christmasflix/views.py
+++ christmasflix/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at last christmas.")
+
+
+def detail(request, list_id):
+    return HttpResponse("You're looking at lists %s" % list_id)
+
+def results(request, list_id):
+    response = "You're looking at the results of list %s."
+    return HttpResponse(response % list_id)
+
+def vote(request, list_id):
+    return HttpResponse("You're voting on question %s." % list_id)
