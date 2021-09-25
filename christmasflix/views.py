2c849a52a862ba280829a4d44afeef0b1fb05c73
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-25 16:16:55 +0200

    Index template working

--- christmasflix/views.py
+++ christmasflix/views.py
@@ -1,22 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
 from .models import MovieList, Movie
 
 
 def index(request):
-    latest_movie_list = MovieList.objects.order_by('id')[:5]
-    context = {'movie list': latest_movie_list}
+    users = MovieList.objects.all()
+    context = {'movie_lists': users}
     return render(request, 'christmasflix/index.html', context)
 
 
 def detail(request, movielist_id):
+    # view list of choices
     """
     movie_list = get_object_or_404(MovieList, pk=movielist_id)
     return render(request, 'christmasflix/detail.html', {'Lists': movie_list})
-    """
-    view_lists = get_object_or_404(MovieList, pk=movielist_id)
     return HttpResponse("You're looking at the list for %s." % view_lists)
+    """
+    #view_movies = get_object_or_404(MovieList, pk=movielist_id)
+    view_movies = Movie.objects.get(id=movielist_id)
+    template = loader.get_template('christmasflix/detail.html')
+    context = {
+        'latest_question_list': view_movies,
+    }
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'christmasflix/detail.html', {'Lists': view_movies})
+
+
 
 
 def results(request, movielist_id):
