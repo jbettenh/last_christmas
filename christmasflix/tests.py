9221acc313de564e2fd93d2d555eae9020893689
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-09-30 21:00:23 +0200

    Added test for index template usage

--- christmasflix/tests.py
+++ christmasflix/tests.py
@@ -3,6 +3,7 @@ from django.shortcuts import reverse
 
 from .models import MovieList
 
+
 class MovieListIndexViewTests(TestCase):
     def test_no_lists(self):
         response = self.client.get(reverse('christmasflix:index'))
@@ -16,13 +17,19 @@ class MovieListIndexViewTests(TestCase):
 
         self.assertQuerysetEqual(response.context['movie_lists'], [mylist1])
 
-
     def test_two_movie_lists(self):
         mylist1 = MovieList.objects.create(name="mylist1")
         mylist2 = MovieList.objects.create(name="mylist2")
         response = self.client.get(reverse('christmasflix:index'))
 
         self.assertQuerysetEqual(response.context['movie_lists'], [mylist1, mylist2], ordered=False)
+
+
+class IndexViewTest(TestCase):
+    def test_index_uses_template(self):
+        response = self.client.get(f'/christmasflix/')
+
+        self.assertTemplateUsed(response, 'christmasflix/index.html')
 
 
 class DetailViewTest(TestCase):
