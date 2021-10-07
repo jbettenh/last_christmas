a922c6cabded49c7747f4b8f928944eb01fb12e4
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-10-07 07:54:17 +0200

    Store image urls in model

--- christmasflix/models.py
+++ christmasflix/models.py
@@ -16,6 +16,7 @@ class MovieList(models.Model):
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.TextField(default='')
+    img_url = models.TextField(default='')
     movielist = models.ForeignKey(MovieList, default=None, on_delete=models.CASCADE,)
 
     def __str__(self):
