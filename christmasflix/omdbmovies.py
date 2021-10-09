f5b5fc11ff5bb5b464d9394614b367c9784e5d0b
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-10-09 08:28:31 +0200

    Hard-coded API call

--- christmasflix/omdbmovies.py
+++ christmasflix/omdbmovies.py
@@ -4,3 +4,17 @@ import yaml
 
 BASE_URL = 'http://www.omdbapi.com/?t='
 NOT_FOUND = 'Not found'
+
+
+def search_movie():
+    with open("config.yaml", 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    payload = {'apikey': cfg['api_key'], 't': 'a+christmas+carol'}
+
+    response = requests.get(BASE_URL, params=payload)
+    return response.text
+
+
+if __name__ == '__main__':
+    print(search_movie())
