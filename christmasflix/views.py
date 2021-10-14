a57ff6d9cff1be15c9efa1fab7979abe5b1d3082
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-10-14 20:49:33 +0200

    Issue on deleting movie

--- christmasflix/views.py
+++ christmasflix/views.py
@@ -39,7 +39,7 @@ def add_movie(request, movielist_id):
 def delete_movie(request, movie_id):
     current_movie = Movie.objects.get(id=movie_id)
     current_movie.delete()
-    return redirect(reverse('christmasflix:detail', args=(25,)))
+    return redirect(reverse('christmasflix:detail', args=(29,)))
 
 
 class DetailView(generic.DetailView):
