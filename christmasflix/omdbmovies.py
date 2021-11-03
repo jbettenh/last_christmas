e573e97ac1b91e07dec1f048dbe58c9eda9aaa75
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-11-03 20:59:42 +0100

    Add movie by imdbID

--- christmasflix/omdbmovies.py
+++ christmasflix/omdbmovies.py
@@ -14,11 +14,11 @@ def search_movie(term):
     return requests.get(BASE_URL, params=payload).json()
 
 
-def get_movie_info(title):
+def get_movie_info(movie_id):
     with open("C:\code\python3\last_christmas\christmasflix\config.yaml", 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
-    payload = {'apikey': cfg['api_key'], 't': title}
+    payload = {'apikey': cfg['api_key'], 'i': movie_id}
 
     return requests.get(BASE_URL, params=payload).json()
 
@@ -26,8 +26,8 @@ def get_movie_info(title):
 if __name__ == '__main__':
     movie_info = search_movie('home alone')
     for movie in movie_info['Search']:
-        print(movie['Title'])
+        print(movie['Title'], movie['imdbID'])
 
-    one_movie = get_movie_info('Home Alone 2')
+    one_movie = get_movie_info('tt0099785')
     print(one_movie)
 
