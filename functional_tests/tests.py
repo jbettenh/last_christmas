1b94242bcc8f3d9ab53e705d59bb6271f9d14e4d
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-07-23 11:01:30 +0200

    Make movie list horizontal

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.staticfiles.testing import LiveServerTestCase
 from django.contrib.auth import get_user_model
+from unittest import skip
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -54,3 +55,13 @@ class NewVistorTest(LiveServerTestCase):
         self.assertEqual('Die Hard', self.browser.find_element(By.ID, 'movie-poster').get_attribute("alt"))
 
 
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentallz tries to submit an empty list item
+        # She hits enter on the empty list box
+        # The home page refreshes, and there is an error message saying that list items cannot be blank
+        # She tries again with some text for the item, which now works
+        # Perversely, she now decides to submit a second blank list item
+        # She receives a similar warning on the list page
+        # And she can correct it by filling some text in
+        self.fail('write me!')
